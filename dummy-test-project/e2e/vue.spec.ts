8197250b4ed8d42b5353772efd50fb806f79fda3
XanderStrouwi <xander.vanherbergen@strouwi.be>
2023-10-30 16:27:12 +0100

    run npm test:e2e

--- dummy-test-project/e2e/vue.spec.ts
+++ dummy-test-project/e2e/vue.spec.ts
@@ -3,10 +3,8 @@ import { test, expect } from '@playwright/test';
 // See here how to get started:
 // https://playwright.dev/docs/intro
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('/');
-});
 
 test('visits the app root url', async ({ page }) => {
+  await page.goto('/');
   await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
 })
