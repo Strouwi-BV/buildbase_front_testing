afd56a17dcae4f66ccc53c11c6ebe21931406ad5
XanderStrouwi <xander.vanherbergen@strouwi.be>
2023-10-30 16:06:38 +0100

    Go to page in beforeach

--- dummy-test-project/e2e/vue.spec.ts
+++ dummy-test-project/e2e/vue.spec.ts
@@ -2,7 +2,11 @@ import { test, expect } from '@playwright/test';
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
+
+test.beforeEach(async ({ page }) => {
   await page.goto('/');
+});
+
+test('visits the app root url', async ({ page }) => {
   await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
 })
