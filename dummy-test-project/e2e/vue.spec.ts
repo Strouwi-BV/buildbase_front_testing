1de236a3ca9739657b6a2898eb19b2cf807c906f
XanderStrouwi <xander.vanherbergen@strouwi.be>
2023-10-31 16:25:36 +0100

    Screenshots?

--- dummy-test-project/e2e/vue.spec.ts
+++ dummy-test-project/e2e/vue.spec.ts
@@ -4,14 +4,16 @@ import { test, expect } from '@playwright/test';
 // https://playwright.dev/docs/intro
 
 
-test('visits the app root url', async ({ page }) => {
+test('visits the app root url', async ({ page }, testInfo) => {
   await page.goto('/', {waitUntil: 'domcontentloaded'});
   await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
-  await page.screenshot({ path: 'screenshot.png' });
+  const screenshot = await page.screenshot({ path: 'screenshot.png' });
+  await testInfo.attach('visits_the_app_root_url', { body: screenshot, contentType: 'image/png' })
 })
 
 
-test('fails', async ({ page }) => {
+test('fails', async ({ page }, testInfo) => {
   await expect(true).toBeFalsy();
-  await page.screenshot({ path: 'screenshot.png' });
+  const screenshot = await page.screenshot({ path: 'screenshot.png' });
+  await testInfo.attach('fails', { body: screenshot, contentType: 'image/png' })
 })
