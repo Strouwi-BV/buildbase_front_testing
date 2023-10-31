8eba5bbd7336c86c3658500d1b344390cfdd97af
XanderStrouwi <xander.vanherbergen@strouwi.be>
2023-10-31 09:46:33 +0100

    Made test not always true anymore

--- dummy-test-project/e2e/vue.spec.ts
+++ dummy-test-project/e2e/vue.spec.ts
@@ -4,10 +4,7 @@ import { test, expect } from '@playwright/test';
 // https://playwright.dev/docs/intro
 
 
-// test('visits the app root url', async ({ page }) => {
-//   await page.goto('/', {waitUntil: 'domcontentloaded'});
-//   await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
-// })
-test('just succeed', async () => {
-  await expect(true).toBeTruthy()
+test('visits the app root url', async ({ page }) => {
+  await page.goto('/', {waitUntil: 'domcontentloaded'});
+  await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
 })
