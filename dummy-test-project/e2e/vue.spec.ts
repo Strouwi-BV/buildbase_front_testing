77c1b440d148dcbe6f60126c29ac3ddcbf8cae4c
XanderStrouwi <xander.vanherbergen@strouwi.be>
2023-11-03 16:08:40 +0100

    Added a test for azure dev

--- dummy-test-project/e2e/vue.spec.ts
+++ dummy-test-project/e2e/vue.spec.ts
@@ -4,3 +4,13 @@ test('visits the app root url', async ({ page }) => {
   await page.goto('/', {waitUntil: 'domcontentloaded'});
   await expect(page.locator('div.greetings > h1')).toHaveText('You did it!');
 })
+
+test.describe('azure dev', () => {
+  test.use({baseURL: 'https://backoffice-dev.buildbase.be'});
+  
+  test('visits buildbase dev login', async ({ page }) => {
+    await page.goto('/login', {waitUntil: 'domcontentloaded'});
+    await expect(page.locator('h1')).toHaveText('Welkom bij');
+  })
+})
+  
