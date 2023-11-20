f58e907a077eca70569a4473e74a05cd7de62e74
Tom Peeters <tom.peeters@strouwi.be>
2023-11-20 19:25:18 +0100

    remove some more leftovers of dummy project, add html reporting, use latest node version on azure

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,12 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['junit', { outputFile: 'test-results/e2e-junit-results.xml' }]],
+  reporter: [
+      // junit reporter is used for publishing test results to Azure devops
+      ['junit', { outputFile: 'test-results/results.xml' }],
+      // html report is used for an 'easy understandable file' during local run
+      ['html']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
