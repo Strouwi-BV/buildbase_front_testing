dde0dc92abcb2b729252b20b536794ebab365a40
Thibe Vlasselaerts <r0932716@ucll.be>
2024-04-15 11:28:36 +0200

    txt file toegevoegd

--- i&t/basic-test.spec.ts
+++ i&t/basic-test.spec.ts
@@ -1,11 +1,10 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test.describe('azure dev', () => {
-  test.use({baseURL: 'https://backoffice-dev.buildbase.be'});
-  
-  test('visits buildbase dev login', async ({ page }) => {
-    await page.goto('/login', {waitUntil: 'domcontentloaded'});
-    await expect(page.locator('h1')).toHaveText('Welkom bij');
-  })
-})
-  
+test.describe("azure dev", () => {
+  test.use({ baseURL: "https://backoffice-dev.buildbase.be" });
+
+  test("visits buildbase dev login", async ({ page }) => {
+    await page.goto("/login", { waitUntil: "domcontentloaded" });
+    await expect(page.locator("h1")).toHaveText("Welkom bij");
+  });
+});
