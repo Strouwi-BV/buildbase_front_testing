6ca9aa9bf637ce4ecab55c3e1408ba1e402664c4
dkyan33 <kyan.decerf@hotmail.be>
2024-04-17 13:51:11 +0200

    trial registration test in correct file

---
+++ i&t/authentication/trialRegistration.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+const generateRandomEmail = require("../Utils/generateEmail");
+
+test.describe('Trial subscription', () => {
+    test.use({baseURL: 'https://backoffice-dev.buildbase.be'});
+   
+  
+    test('Trial subscription account', async ({ page }) => {
+      await page.goto('https://backoffice-dev.buildbase.be/');
+      await page.getByRole('link', { name: 'Registreer.' }).click();
+      await page.locator("(//input[@type='text'])[1]").click();
+      await page.locator("(//input[@type='text'])[1]").fill('buildByMe');
+      await page.locator("(//input[@type='text'])[2]").click();
+      await page.locator("(//input[@type='text'])[2]").fill('Kyan');
+      await page.locator("(//input[@type='text'])[3]").click();
+      await page.locator("(//input[@type='text'])[3]").fill('Decerf');
+      await page.locator("(//input[@type='email'])[1]").click();
+      const randomEmail = generateRandomEmail();
+      await page.locator("(//input[@type='email'])[1]").fill(randomEmail);
+      await page.locator("(//input[@type='password'])[1]").click();
+      await page.locator("(//input[@type='password'])[1]").fill('Test123');
+      await page.locator("(//input[@tabindex='NaN'])[1]").click();
+      await page.locator("(//input[@tabindex='NaN'])[1]").fill('Test123');
+      await page.getByText('Gratis proefversie').click();
+      await page.locator("(//div[@class='v-input--selection-controls__ripple'])[2]").first().click();
+      await page.getByRole('button', { name: 'Start 30 dagen gratis' }).click();
+      await page.waitForNavigation({ timeout: 5000 });
+      await expect(page.url()).toBe("https://backoffice-dev.buildbase.be/calendar")
+    });
+  })
