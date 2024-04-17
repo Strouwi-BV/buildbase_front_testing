7ec07ea9d6269b2d3688056f328cfb60b9c9736b
dkyan33 <kyan.decerf@hotmail.be>
2024-04-17 07:42:48 +0200

    ID's aangepast naar xpath locators

--- i&t/basic-test.spec.ts
+++ i&t/basic-test.spec.ts
@@ -16,20 +16,20 @@ test.describe('Trial subscription', () => {
   test('Trial subscription account', async ({ page }) => {
     await page.goto('https://backoffice-dev.buildbase.be/');
     await page.getByRole('link', { name: 'Registreer.' }).click();
-    await page.locator('#input-58').click();
-    await page.locator('#input-58').fill('buildByMe');
-    await page.locator('#input-59').click();
-    await page.locator('#input-59').fill('Kyan');
-    await page.locator('#input-60').click();
-    await page.locator('#input-60').fill('Decerf');
-    await page.locator('#input-61').click();
-    await page.locator('#input-61').fill('dkyan007@gmail.com');
-    await page.locator('#input-63').click();
-    await page.locator('#input-63').fill('Test123');
-    await page.locator('#confirmPassword').click();
-    await page.locator('#confirmPassword').fill('Test123');
+    await page.locator("(//input[@type='text'])[1]").click();
+    await page.locator("(//input[@type='text'])[1]").fill('buildByMe');
+    await page.locator("(//input[@type='text'])[2]").click();
+    await page.locator("(//input[@type='text'])[2]").fill('Kyan');
+    await page.locator("(//input[@type='text'])[3]").click();
+    await page.locator("(//input[@type='text'])[3]").fill('Decerf');
+    await page.locator("(//input[@type='email'])[1]").click();
+    await page.locator("(//input[@type='email'])[1]").fill('dkyan007@gmail.com');
+    await page.locator("(//input[@type='password'])[1]").click();
+    await page.locator("(//input[@type='password'])[1]").fill('Test123');
+    await page.locator("(//input[@tabindex='NaN'])[1]").click();
+    await page.locator("(//input[@tabindex='NaN'])[1]").fill('Test123');
     await page.getByText('Gratis proefversie').click();
-    await page.locator('.v-input__slot > .v-input--selection-controls__input > .v-input--selection-controls__ripple').first().click();
+    await page.locator("(//div[@class='v-input--selection-controls__ripple'])[2]").first().click();
     await page.getByRole('button', { name: 'Start 30 dagen gratis' }).click();
     await page.waitForNavigation({ timeout: 5000 });
     await expect(page.url()).toBe("https://backoffice-dev.buildbase.be/calendar")
