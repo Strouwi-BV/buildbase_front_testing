879e147290a5477868892af5755b2738f39bcbcb
JarneGuns <jarne.guns@student.ucll.be>
2024-04-19 15:18:44 +0200

    teamTest BT13 completed

---
+++ i&t/Utils/randomString.ts
@@ -0,0 +1,11 @@
+function generateRandomString() {
+  const maxLength = 10;
+  const randomString =
+    "Test" +
+    Math.random()
+      .toString(36)
+      .substring(2, maxLength + 2);
+  return randomString;
+}
+
+module.exports = generateRandomString;
