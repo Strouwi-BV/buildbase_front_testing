1eb518f0ae92db9a581ee40e644e7842a87e146f
RuneTheunis <rune.theunis@student.ucll.be>
2024-04-22 15:30:15 +0200

    BT-22-23-24 toegevoegd

---
+++ i&t/employees/FilterAndSearchEmpoyees.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+const { login } = require('../Utils/login')
+const generateRandomString = require('../Utils/randomString');
+
+test.beforeEach('Login', async ({ page }) => {
+    await login(page);
+});
+
+test('FilterAndSearch', async ({ page }) => {
+    test.setTimeout(180000);
+    await page.locator("(//div[contains(text(),'Werknemers')])[1]").click();
+    await page.locator("(//button[@class='tertiary text-none v-btn v-btn--outlined theme--light v-size--default mr-2'])[1]").click();
+    await page.waitForTimeout(1000);
+    await page.locator("(//label[normalize-space()='Actief'])[1]").click();
+    await page.waitForTimeout(1000);
+    await page.locator("(//label[normalize-space()='Actief'])[1]").click();
+    await page.locator("(//label[normalize-space()='Inactief'])[1]").click();
+    await page.waitForTimeout(1000);
+    await page.locator("(//label[normalize-space()='Inactief'])[1]").click();
+    await page.locator("(//label[normalize-space()='Leutig'])[1]").click();
+    await page.waitForTimeout(1000);
+    await page.locator("(//label[normalize-space()='Leutig'])[1]").click();
+    await page.locator("(//input[@type='search'])[1]").click();
+    await page.locator("(//input[@type='search'])[1]").fill('kyan');
+})
