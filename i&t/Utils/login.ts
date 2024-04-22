647b052de7a587403039ed98fc0792d5b2f2766e
RuneTheunis <rune.theunis@student.ucll.be>
2024-04-22 13:06:20 +0200

    BT-20 toegevoegd

--- i&t/Utils/login.ts
+++ i&t/Utils/login.ts
@@ -2,11 +2,14 @@ async function login(page) {
 
   await page.goto('https://backoffice-dev.buildbase.be/login');
   await page.getByLabel('Email').click();
-  await page.getByLabel('Email').fill('rune.theunis@student.ucll.be');
+  await page.getByLabel('Email').fill('Kyan.decerf@student.ucll.be');
   await page.getByLabel('Wachtwoord', { exact: true }).click();
-  await page.getByLabel('Wachtwoord', { exact: true }).fill('Test321');
+  await page.getByLabel('Wachtwoord', { exact: true }).fill('Test123');
   await page.getByRole('button', { name: 'Inloggen' }).click();
   await page.waitForNavigation();
+
+
+
 }
 
 module.exports = {
--- i&t/authentication/passwordChange.spec.ts
+++ i&t/authentication/passwordChange.spec.ts
@@ -11,10 +11,22 @@ test('ChangePassword', async ({ page }) => {
     await page.locator("(//div[@class='v-card__title text-h6 small-caps font-weight-bold pl-1'])[1]").click();
     await page.locator("(//span[normalize-space()='WIJZIG WACHTWOORD'])[1]").click();
     await page.locator("(//input[@type='password'])[1]").click();
-    await page.locator("(//input[@type='password'])[1]").fill('Test321');
+    await page.locator("(//input[@type='password'])[1]").fill('Test123');
     await page.locator("(//input[@type='password'])[2]").click();
     await page.locator("(//input[@type='password'])[2]").fill('Test321');
     await page.locator("(//input[@type='password'])[3]").click();
     await page.locator("(//input[@type='password'])[3]").fill('Test321');
+    await page.locator("(//span[normalize-space()='Opslaan'])[1]").click();
+
+    await page.waitForTimeout(2000);
+    //reset
+    await page.locator("(//div[@class='v-card__title text-h6 small-caps font-weight-bold pl-1'])[1]").click();
+    await page.locator("(//span[normalize-space()='WIJZIG WACHTWOORD'])[1]").click();
+    await page.locator("(//input[@type='password'])[1]").click();
+    await page.locator("(//input[@type='password'])[1]").fill('Test321');
+    await page.locator("(//input[@type='password'])[2]").click();
+    await page.locator("(//input[@type='password'])[2]").fill('Test123');
+    await page.locator("(//input[@type='password'])[3]").click();
+    await page.locator("(//input[@type='password'])[3]").fill('Test123');
     await page.locator("(//span[normalize-space()='Opslaan'])[1]").click();
 })
