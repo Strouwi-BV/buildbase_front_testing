e049b835d8c0880b0d49bfea1188ffcc106a179e
RuneTheunis <rune.theunis@student.ucll.be>
2024-04-25 15:51:53 +0200

    zonder delete

--- i&t/clientAndProject/createAndEditClient.spec.ts
+++ i&t/clientAndProject/createAndEditClient.spec.ts
@@ -93,7 +93,7 @@ test('Create/edit client', async ({ page }) => {
     .first()
     .click();
 
-  await page.getByRole('button', { name: 'Acties' }).click();
-  await page.getByText('Verwijder').click();
-  await page.getByRole('button', { name: 'Bevestig' }).click();
+  //await page.getByRole('button', { name: 'Acties' }).click();
+  //await page.getByText('Verwijder').click();
+  //await page.getByRole('button', { name: 'Bevestig' }).click();
 });
