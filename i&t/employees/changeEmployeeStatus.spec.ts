334cd037cd4b83a76ef599679bf466173344d0af
dkyan33 <kyan.decerf@hotmail.be>
2024-04-26 14:21:11 +0200

    change employee status test working

--- i&t/employees/changeEmployeeStatus.spec.ts
+++ i&t/employees/changeEmployeeStatus.spec.ts
@@ -6,48 +6,52 @@ test.beforeEach("Login", async ({ page }) => {
   await login(page);
 });
 
-test("addEmployeeActiveTest", async ({ page }) => {
+test("addEmployeeActiveTest", async ({browserName, page }) => {
   await page.getByRole("link", { name: "Werknemers" }).click();
 
-  // Definieer de XPath voor het actieve en inactieve element
-  const activeXPath =
-    "(//span[@class='v-chip__content'][normalize-space()='actief'])";
-  const inactiveXPath =
-    "(//span[@class='v-chip__content'][normalize-space()='inactief'])";
-
-  let i = 1;
-  let activeElement, inactiveElement;
-
-  // Blijf zoeken naar actieve en inactieve elementen totdat ze niet meer worden gevonden
-  while (true) {
-    activeElement = await page.$(`${activeXPath}[${i}]`);
-    inactiveElement = await page.$(`${inactiveXPath}[${i}]`);
-
-    // Als zowel het actieve als het inactieve element niet worden gevonden, stop dan de lus
-    if (!activeElement && !inactiveElement) {
-      break;
-    }
-
-    // Controleer of het actieve element aanwezig is en zichtbaar is
-    if (activeElement) {
-      console.log(`Element ${i} is actief`);
-      // Voer hier acties uit voor het actieve element, bijvoorbeeld klikken op het mannetje
-      await activeElement.click();
-    }
-
-    // Controleer of het inactieve element aanwezig is en zichtbaar is
-    if (inactiveElement) {
-      console.log(`Element ${i} is inactief`);
-      // Hier actie voor het inactieve element
-    }
-
-    i++;
+  if (browserName === 'chromium') {
+  await page.waitForURL("https://backoffice-dev.buildbase.be/users");
+  await page.waitForTimeout(10000)
+  await expect(page.locator('tbody')).toContainText('actief');
+  await page.locator("(//span[@class='v-chip__content'][normalize-space()='actief'])[1]").click();
+  await expect(page.getByRole('main')).toContainText('Actieve gebruiker');
+  await page.getByRole('button', { name: 'Acties' }).click();
+  await page.getByText('Zet op inactief').click();
+  await page.getByRole('button', { name: 'Bevestig' }).click();
+  await expect(page.getByRole('main')).toContainText('Inactieve gebruiker');
+  await page.getByRole('button', { name: 'Acties' }).click();
+  await page.getByText('Zet op actief').click();
+  await page.getByRole('button', { name: 'Bevestig' }).click();
   }
-
-  const addedMessage = await page
-    .locator(
-      "(//div[contains(text(),'Werknemer werdt succesvol aangemaakt')])[1]"
-    )
-    .innerText();
-  expect(addedMessage).toContain("Werknemer werdt succesvol aangemaakt");
+  if (browserName === 'firefox') {
+    await page.waitForTimeout(10000);
+    await page.waitForURL("https://backoffice-dev.buildbase.be/users");
+    await page.waitForTimeout(10000)
+    await expect(page.locator('tbody')).toContainText('actief');
+    await page.locator("(//span[@class='v-chip__content'][normalize-space()='actief'])[1]").click();
+    await expect(page.getByRole('main')).toContainText('Actieve gebruiker');
+    await page.getByRole('button', { name: 'Acties' }).click();
+    await page.getByText('Zet op inactief').click();
+    await page.getByRole('button', { name: 'Bevestig' }).click();
+    await expect(page.getByRole('main')).toContainText('Inactieve gebruiker');
+    await page.getByRole('button', { name: 'Acties' }).click();
+    await page.getByText('Zet op actief').click();
+    await page.getByRole('button', { name: 'Bevestig' }).click();
+    }
+    if (browserName === 'webkit') {
+      await page.waitForTimeout(20000);
+      await page.waitForURL("https://backoffice-dev.buildbase.be/users");
+      await page.waitForTimeout(10000)
+      await expect(page.locator('tbody')).toContainText('actief');
+      await page.locator("(//span[@class='v-chip__content'][normalize-space()='actief'])[1]").click();
+      await expect(page.getByRole('main')).toContainText('Actieve gebruiker');
+      await page.getByRole('button', { name: 'Acties' }).click();
+      await page.getByText('Zet op inactief').click();
+      await page.getByRole('button', { name: 'Bevestig' }).click();
+      await expect(page.getByRole('main')).toContainText('Inactieve gebruiker');
+      await page.getByRole('button', { name: 'Acties' }).click();
+      await page.getByText('Zet op actief').click();
+      await page.getByRole('button', { name: 'Bevestig' }).click();
+      }
 });
+
