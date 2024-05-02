7317af8b4f153ae03b37fe7e2b5b8d6a6f8066b9
JarneGuns <jarne.guns@student.ucll.be>
2024-05-02 12:27:37 +0200

    Fixed the TeamTest

--- i&t/Utils/login.ts
+++ i&t/Utils/login.ts
@@ -1,7 +1,7 @@
 async function login(page) {
   await page.goto("https://backoffice-dev.buildbase.be/login");
   await page.getByLabel("Email").click();
-  await page.getByLabel("Email").fill("kyan.decerf@student.ucll.be");
+  await page.getByLabel("Email").fill("jarneguns@gmail.com");
   await page.getByLabel("Wachtwoord", { exact: true }).click();
   await page.getByLabel("Wachtwoord", { exact: true }).fill("Test123");
   await page.getByRole("button", { name: "Inloggen" }).click();
